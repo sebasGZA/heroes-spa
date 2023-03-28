808dd9290439d4d31929bbb472d3e14418d7328d
sebasGZA <sebassga34@gmail.com>
2023-03-27 21:21:10 -0500

    Privates routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRouter } from "../heroes";
+import { PrivateRouter } from "./PrivateRouter";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRouter />} />
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRouter>
+              <HeroesRouter />
+            </PrivateRouter>
+          }
+        />
       </Routes>
     </>
   );
