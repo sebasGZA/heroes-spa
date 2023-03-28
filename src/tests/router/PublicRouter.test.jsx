eb3e46b5a7b79247631c7816aca0e5d59dda0a60
sebasGZA <sebassga34@gmail.com>
2023-03-27 22:41:52 -0500

    Tests: <PublicRouter />

---
+++ src/tests/router/PublicRouter.test.jsx
@@ -0,0 +1,55 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+
+import { AuthContext } from "../../auth/context";
+import { PublicRouter } from "../../router/PublicRouter";
+
+describe("Test inn <PublicRouter />", () => {
+  test("should show children if it is not authenticated", () => {
+    const contextValue = {
+      logged: false,
+    };
+
+    const { container } = render(
+      <AuthContext.Provider value={contextValue}>
+        <PublicRouter>
+          <h1>Public routes</h1>
+        </PublicRouter>
+      </AuthContext.Provider>
+    );
+
+    expect(container).toMatchSnapshot();
+    expect(screen.getByText("Public routes")).toBeTruthy();
+  });
+
+  test("should navigate if it is authenticated", () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        id: 123,
+        name: "Sebas",
+      },
+    };
+
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter initialEntries={["/login"]}>
+          <Routes>
+            <Route
+              path="/login"
+              element={
+                <PublicRouter>
+                  <h1>Public routes</h1>
+                </PublicRouter>
+              }
+            />
+
+            <Route path="/marvel" element={<h1>marvel page</h1>} />
+          </Routes>
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    expect(screen.getByText("marvel page")).toBeTruthy();
+  });
+});
