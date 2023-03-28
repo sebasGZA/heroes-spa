857d663585a3a2325ab83d1dd74629cb8c9f7c3b
sebasGZA <sebassga34@gmail.com>
2023-03-28 10:40:00 -0500

    <SearchPage />

--- src/tests/heroes/pages/SearchPage.test.jsx
+++ src/tests/heroes/pages/SearchPage.test.jsx
@@ -1,8 +1,17 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { SearchPage } from "../../../heroes/pages/SearchPage";
 
+const mockedUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUseNavigate,
+}));
+
 describe("Tests in <SearchPage />", () => {
+  beforeEach(() => jest.clearAllMocks());
+
   test("should show making match with snapshow", () => {
     const { container } = render(
       <MemoryRouter>
@@ -22,10 +31,39 @@ describe("Tests in <SearchPage />", () => {
     const inputSearch = screen.getByRole("textbox");
     expect(inputSearch.value).toBe("batman");
 
-    const img = screen.getByRole("img")
-    expect(img.src).toBe("http://localhost/src/assets/heroes/dc-batman.jpg")
-  
-     const alert = screen.getByLabelText("alert-danger")
-     expect(alert.style.display).toBe("none")   
-});
+    const img = screen.getByRole("img");
+    expect(img.src).toBe("http://localhost/src/assets/heroes/dc-batman.jpg");
+
+    const alert = screen.getByLabelText("alert-danger");
+    expect(alert.style.display).toBe("none");
+  });
+
+  test("should show an error if error does not exist", () => {
+    render(
+      <MemoryRouter initialEntries={["/search?q=safa"]}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+
+    const alert = screen.getByLabelText("alert-danger");
+    expect(alert.style.display).toBe("block");
+  });
+
+  test("should call the navigate to new screen", () => {
+    render(
+      <MemoryRouter initialEntries={["/search"]}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+
+    const inputSearch = screen.getByRole("textbox");
+    fireEvent.change(inputSearch, {
+      target: { name: "searchText", value: "safa" },
+    });
+
+    const buttonSearch = screen.getByRole("button");
+    fireEvent.click(buttonSearch);
+
+    expect(mockedUseNavigate).toHaveBeenCalledWith("?q=safa");
+  });
 });
